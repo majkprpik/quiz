03c74971bba59a80e84e8363dfe1b978d4df9d0b
majkprpik <majkprpik@users.noreply.github.com>
2023-01-13 12:19:13 +0100

    swagger for prod

--- Program.cs
+++ Program.cs
@@ -53,11 +53,8 @@ var app = builder.Build();
 
 app.MapServerSentEvents("sse/rn-updates");
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
